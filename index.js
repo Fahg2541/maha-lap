a30f01c8d8dc6364b0d921a9a3d4a15695c27a61
Fahg2541 <Fahg2541@gmail.com>
2019-09-22 13:45:51 +0700

    updeat

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('เจริญพรจ้า!!!!'))
+app.get('/', (req, res) => res.send("เจริญพรจ้า!!!!"));
+app.get("/namo", (req, res) => {
+    const day = req.query.day;
+    const story = req.query.story;
+    const time = req.query.time;
+    const theHolyNumber = MahaLap.namo(day, story, time);
+    res.json({
+        result: theHolyNumber
+    });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
