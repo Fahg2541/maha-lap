d8ea078f88150eafe3ababed5adab67374ce9912
Fahg2541 <Fahg2541@gmail.com>
2019-09-22 10:59:21 +0700

    updeat maha-lap

--- maha-lap.js
+++ maha-lap.js
@@ -1,29 +1,29 @@
 class MahaLap {
     static tamnaiWithDay(day) {
         switch (day) {
-            case อาทิตย์:
-                return ("1");
-            case จันทร์:
-                return ("2");
-            case อังคาร:
-                return ("3");
-            case พุธ:
-                return ("4");
-            case พฤหัสบดี:
-                return ("5");
-            case ศุกร์:
-                return ("6");
-            case เสาร์:
-                return ("7");
-            case สงกรานต์:
-                return ("13");
-            case ฮาโลวีน:
-                return ("31");
-            case คริสมาสต์:
-                return ("25  ");
+            case "อาทิตย์":
+                return 1;
+            case "จันทร์":
+                return 2;
+            case "อังคาร":
+                return 3;
+            case "พุธ":
+                return 4;
+            case "พฤหัสบดี":
+                return 5;
+            case "ศุกร์":
+                return 6;
+            case "เสาร์":
+                return 7;
+            case "สงกรานต์":
+                return 13;
+            case "ฮาโลวีน":
+                return 31;
+            case "คริสมาสต์":
+                return 25;
 
             default:
-                return day;
+                return 99;
         }
 
 
