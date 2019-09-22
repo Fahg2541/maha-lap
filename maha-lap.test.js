de8f17c39dd8f7f38e49e5f441d79fdecfea7e64
Fahg2541 <Fahg2541@gmail.com>
2019-09-22 10:24:44 +0700

    update maha-lap-test

--- maha-lap.test.js
+++ maha-lap.test.js
@@ -3,6 +3,6 @@ const MahaLap = require("./maha-lap");
 describe("Test Maha-Lap", () => {
     test("Test Tamnai with day", () => {
         const result = MahaLap.tamnaiWithDay("จันทร์");
-        expect(result).toEqual(2);
+        expect(result).toEqual(3);
     });
 });
